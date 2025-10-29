55d34cc0fa5c63d341376cc0912e041c4044c8f1
Ken Española <revas4192@gmail.com>
2025-10-29 17:06:01 +0800

    initial commit

---
+++ frontend/src/pages/homepage.tsx
@@ -0,0 +1,9 @@
+// src/pages/Homepage.tsx
+export default function Homepage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello World</h1>
+      <p>Welcome to your React + TypeScript app!</p>
+    </div>
+  )
+}
